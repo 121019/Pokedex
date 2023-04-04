2512360bab92e71f764e4b0cc8b976257bb74002
Cathy A <ptipoison@yahoo.fr>
2023-04-04 21:33:39 +0200

    React basics 02

---
+++ src/components/PokemonCard.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+
+const PokemonCard = () => {
+    return (
+        <figure>
+            <img src = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="Pokemon Bulbasaur" />
+        </figure>
+    );
+}
+
+
+export default PokemonCard ;
