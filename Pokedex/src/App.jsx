7cb190ef33f7e930b88d6fab54b5a157746345dd
121019 <122298321+121019@users.noreply.github.com>
2023-04-08 21:05:15 +0200

    React Basics 07 bis

--- Pokedex/src/App.jsx
+++ Pokedex/src/App.jsx
@@ -2,24 +2,40 @@
 import './App.css'
 import PokemonCard from './components/PokemonCard.jsx'
 import {useState} from "react"
-
+import { useEffect } from 'react'
 
 
 const pokemonList = [ 
     {name: "Bulbasaur", imgSrc: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"},
-    {name: "Mew", imgSrc: "" },
+    {name: "Mew", imgSrc: "src/assets/Mew.png" },
     {name: "charmander",imgSrc:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png"},
     {name: "squirtle",imgSrc:"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png"},
     {name: "pikachu", imgSrc: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png"},
 //Pour liste pokemon
-  ];   
+]; 
+ 
+
+
 
 function App() {
+ 
   const [photo, setPhoto] = useState(0);
 
-  const handleNextClick = () => {
+  useEffect(() => {
+    alert('Hello Pokemon Trainer') 
+    }, []);
+
+
+  useEffect(() => {
+    if (pokemonList[photo].name === 'pikachu') {
+      alert('Pika Pikachu !!!');
+    }
+  }, [photo]);
+
+
+  function handleNextClick() {
     setPhoto(photo + 1);
-  };
+  }
 
   const handlePreviewClick = () => {
     setPhoto(photo - 1);
@@ -40,7 +56,7 @@ function App() {
       <PokemonCard pokemon={pokemonList[photo]} />
     </div>
   );
-}
+  }
 
 export default App;
 
